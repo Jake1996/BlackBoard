d6649884ce13d3ea6ddf47eec78bf5f980182778
Jake1996 <jatin15pm@gmail.com>
2015-10-04 17:13:30 +0530

    login page

---
+++ public/login.php
@@ -0,0 +1,36 @@
+<?php require_once("../includes/db_connection.php") ?>
+<?php require_once("../includes/functions.php") ?>
+
+<html>
+<head></head>
+<body>
+<?php	
+	if (isset($_POST['submit'])) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$found_admin = attempt_login($username, $password);
+	if ($found_admin) {
+      		// Success
+		
+		// Mark user as logged in
+		$_SESSION["admin_id"] = $found_admin["id"];
+		$_SESSION["username"] = $found_admin["username"];
+	}
+	else {
+		echo "not logged in";}
+	}
+	if(!isset($_SESSION['admin_id'])) {
+?>
+
+<form  action = "login.php" method = "post">
+<p>Username:
+        <input type="text" name="username" value="" />
+      </p>
+      <p>Password:
+        <input type="password" name="password" value="" />
+      </p>
+      <input type="submit" name="submit" value="Submit" />
+</form>
+<?php } else echo "Logged in"; ?>
+</body>
+</html>
