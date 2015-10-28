71ed52903001a86f78aa567875227c4273edc678
Imran Ahmed Manzoor <imran.manzoor31@gmail.com>
2015-10-28 08:54:08 +0530

    Navbar
    Fixed positioning issue of Navbar

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -1,54 +1,63 @@
-<link rel="stylesheet" type="text/css" href="stylesheets/NavbarStylings.css"> 
+<link rel="stylesheet" type="text/css" href="stylesheets/NavbarStylings.css">
+<link rel="stylesheet" type="text/css" href="../assets/bootstrap/css/bootstrap.min.css">  
 <body>
-<div class = "row">	
-<nav id="mynavbar">
-	<ul>
-		<li><a href="#">Home</a></li>
-		<li><a href="#">Courses</a>
-		<?php
-	        $output ="";
-	        $output .="<ul id = \"navmenu\">";
-	        $current_branch = "";
-	        $current_sem = 0;
-	        $course_set = getAllCourses();
-	        if($course = mysqli_fetch_assoc($course_set))
-	        {
-	          $id = htmlentities(str_replace(' ', '', $course["branch"]));
-	          $current_sem = $course["sem"];
-	          $output .= "<li id = \"{$id}\"><a href =\"#\">". htmlentities($course["branch"])."</a>";
-	          $output .= "<ul>";
-	          $current_branch = $course["branch"];
-	          $output .= "<li><a href = \"#\">Sem {$current_sem}</a><ul>";
-	          $output .= "<li id = \"";
-	          $output .= htmlentities($course["courseCode"]) . "\">";
-	          $output .= "<a href \"#\">". htmlentities($course["courseName"]) . "</a>";
-	          $output .= "</li>";
-	          while($course = mysqli_fetch_assoc($course_set)) {
-	            if($current_branch != $course["branch"]) {
-	            	$id = htmlentities(str_replace(' ', '', $course["branch"]));
-	            	$output .= "</ul></li></ul></li>";
-	            	$output .= "<li id = \"{$id}\"><a href =\"#\">". htmlentities($course["branch"]) ."</a>";
-	          		$output .= "<ul>";
-	            	$current_branch = $course["branch"];
-	            	$current_sem = -1;
-	            }
-	            if($current_sem != $course["sem"]) {
-	            	if($current_sem != -1) {
-	            		$output .= "</ul></li>";
-	            	}
-	            	$current_sem = $course["sem"];
-	            	$output .= "<li><a href=\"#\">Sem {$current_sem}</a><ul>";
-	            }
-	            $output .= "<li id = \"" .htmlentities($course["courseCode"]) ."\">";
-	          	$output .= "<a href \"#\">" . htmlentities($course["courseName"]) . "</a>";
-	         	$output .= "</li>";
-	          }
-	          mysqli_free_result($course_set);
-	        }
-	        $output .= "</ul>";
-	        echo $output;
-	    ?>
-		</li>	
-	</ul>
-</nav>
+<div class = "container-fluid">
+	<div class = "row">
+		<div class = "col-md-12">	
+			<nav id="mynavbar">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Courses</a>
+						<?php
+		    	    		$output ="";
+		    	    		$output .="<ul id = \"navmenu\">";
+	    		    		$current_branch = "";
+	    		    		$current_sem = 0;
+	    	    			$course_set = getAllCourses();
+	    	    			if($course = mysqli_fetch_assoc($course_set))
+		    	    		{
+		    	    			$id = htmlentities(str_replace(' ', '', $course["branch"]));
+	    		    			$current_sem = $course["sem"];
+	    		     			$output .= "<li id = \"{$id}\"><a href =\"#\">". htmlentities($course["branch"])."</a>";
+	    	    	  			$output .= "<ul>";
+	    	      				$current_branch = $course["branch"];
+	    	      				$output .= "<li><a href = \"#\">Sem {$current_sem}</a><ul>";
+	    	      				$output .= "<li id = \"";
+	    	     		 		$output .= htmlentities($course["courseCode"]) . "\">";
+	    	     		 		$output .= "<a href \"#\">". htmlentities($course["courseName"]) . "</a>";
+	    	    	  			$output .= "</li>";
+	    	      				while($course = mysqli_fetch_assoc($course_set)) 
+	        			  		{
+	        	    				if($current_branch != $course["branch"]) 
+	            					{
+	            						$id = htmlentities(str_replace(' ', '', $course["branch"]));
+	            						$output .= "</ul></li></ul></li>";
+	          			  				$output .= "<li id = \"{$id}\"><a href =\"#\">". htmlentities($course["branch"]) ."</a>";
+	          							$output .= "<ul>";
+	     	    	   					$current_branch = $course["branch"];
+	        	    					$current_sem = -1;
+	            					}
+	            					if($current_sem != $course["sem"]) 
+	            					{
+	            						if($current_sem != -1) 
+	            						{
+	            							$output .= "</ul></li>";
+	            						}
+	            						$current_sem = $course["sem"];
+	            						$output .= "<li><a href=\"#\">Sem {$current_sem}</a><ul>";
+	            					}
+	            					$output .= "<li id = \"" .htmlentities($course["courseCode"]) ."\">";
+	          						$output .= "<a href \"#\">" . htmlentities($course["courseName"]) . "</a>";
+	         						$output .= "</li>";
+	         					}
+	          					mysqli_free_result($course_set);
+	        				}
+	        				$output .= "</ul>";
+	        				echo $output;
+	    				?>
+					</li>	
+				</ul>
+			</nav>
+		</div>
+	</div>
 </div>
