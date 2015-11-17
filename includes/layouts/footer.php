fcd141e956d2a5ace30b8afcf7e6edc320073b2d
Imran Ahmed Manzoor <imran.manzoor31@gmail.com>
2015-11-17 19:04:47 +0530

    pp

--- includes/layouts/footer.php
+++ includes/layouts/footer.php
@@ -1,43 +1,4 @@
 
-<link rel="stylesheet" type="text/css" href="../stylesheets/footer.css">
-<div>
-	<table>
-		<th>
-			<td>About</td>
-			<td>Developers</td>
-			<td>Connect</td>
-			<td>Contribute</td>
-		</th>
-		<tr>
-			<td>
-				Acquiring knowledge should have zero restrictions. We made this website on that simple <i>idea</i>.
-				Anyone can add or edit anything on this website, provided you are a responsible user.
-			</td>
-			<td>
-				<ul>
-					<li>Jatin PM</li>
-					<li>Imran A Manzoor</li>
-					<li>Bhargav</li>
-				</ul>
-			</td>
-			<td>
-				<ul>
-					<li>Facebook</li>
-					<li>Twitter</li>
-				</ul>
-			</td>
-			<td>
-				<ul>
-					<li>Volunteer</li>
-					<li>Donate</li>
-				</ul>
-			</td>
-		</tr>
-	</table>
-</div>
-
-</body>
-</html>
 <?php
   // 5. Close database connection
 	if (isset($connection)) {
